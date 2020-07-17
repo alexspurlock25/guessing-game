86b7cd4bc850c74b46be4f7725106c27d9f7f667
Alexander Spurlock <alexanderspurlock@gmail.com>
2020-07-17 17:40:37 -0400

    Updates/Chnages to core app.
    - Removed the redundancy that flooded this program.
    - Added ValueError so that only numbers can be entered.
    - Restructured the app.

--- guess_number.py
+++ guess_number.py
@@ -3,161 +3,121 @@
 
 import random
 
-# variable to hold random number. 0 to 100
-random_number = random.randint(0, 101)
-print(f"New random number is {random_number}")  ## for testing
-
-guess_list = []
-all_guesses = []
-
-isValidInput = False
-user_number = 0
-while not isValidInput:
-    try:
-        #The number we are trying to guess
-        user_number = int(input("Guess a number between 0 and 100: "))
-        isValidInput = True
-    except ValueError: 
-        print("{} is not a valid number.  Please enter another guess".format(user_number))
+user_guesses = []
 
 
-guess_list.append(user_number)
-all_guesses.append(user_number)
+def play(rand_num):
+    print(f"New random number is {rand_num}")  # for testing
 
+    is_valid_input = False
+    user_number = 0
 
-def wrong_number(num):
-    """
+    while not is_valid_input:
 
-    This method has a while loop that makes the user keep guessing.
-    When the user finally guesses right, the loop becomes false,
-    then the number is sent to 'right_number' method to determine the user's next course of action.
+        try:
 
-    This method is 'skipped' if the user guesses right on his/her first try.
+            user_number = int(input("Guess a number between 0 and 100: "))
+            is_valid_input = True
 
-    """
+            user_guesses.append(user_number)
 
-    while random_number != num:
+        except ValueError:
+            print("NOT VALID INPUT. Please enter another guess")
 
-        if num < 0 or num > 100:
+    if user_number == rand_num and len(user_guesses) == 1:
 
-            if num < 0:
-                num = int(input(f"{num} is below specified range. Try again: "))
-                guess_list.append(num)
-                all_guesses.append(num)
-
-            else:
-                num = int(input(f"{num} is above specified range. Try again: "))
-                guess_list.append(num)
-                all_guesses.append(num)
-
-        elif abs(num - random_number) > 0 and abs(num - random_number) <= 5:
-
-            num = int(input(f"{num} IS BURNING: "))
-            guess_list.append(num)
-            all_guesses.append(num)
-
-        elif abs(num - random_number) > 5 and abs(num - random_number) <= 10:
-
-            num = int(input(f"{num} is HOT: "))
-            guess_list.append(num)
-            all_guesses.append(num)
-
-        elif abs(num - random_number) > 10 and abs(num - random_number) <= 20:
-
-            num = int(input(f"{num} is REALLY WARM: "))
-            guess_list.append(num)
-            all_guesses.append(num)
-
-        elif abs(num - random_number) > 20 and abs(num - random_number) <= 30:
-
-            num = int(input(f"{num} is WARMER: "))
-            guess_list.append(num)
-            all_guesses.append(num)
-
-        elif num > random_number:
-
-            num = int(input(f"{num} is too high. Try Again: "))
-            guess_list.append(num)
-            all_guesses.append(num)
-
-        elif num < random_number:
-
-            num = int(input(f"{num} is too low. Try Again: "))
-            guess_list.append(num)
-            all_guesses.append(num)
-
-    return right_number(num)
-
-
-def right_number(num):
-    """
-
-    This method is for situations in which the user's numbers are correct.
-    Whether it's his/her first guess or after guessing several times.
-
-    """
-
-    print(f"{num} is right! Good Job!")
-
-    question = input("Would you like to play again? ").lower()
-    
-    if question in ["y","yes","yeah","yep"]:
-        guess_list.clear()
-
-        new_random_number = random.randint(0, 101)
-        print(f"New random number is {new_random_number}")  # this line is only for testing
-
-        new_user_number = int(input("Okay, let's play again. Enter a number between 0 and 100: "))
-        guess_list.append(new_user_number)
-        all_guesses.append(new_user_number)
-
-        if new_user_number == new_random_number and len(guess_list) == 1:
-
-            return first_time_guess(new_user_number)
-
-        else:
-            return wrong_number(new_user_number)
+        first_time_guess()
 
     else:
-        return print("Have a good day! These are your results!\n")
+
+        start_guessing(user_number, rand_num)
 
 
-def first_time_guess(num):
-    question = input("WOW!! You guessed on your first try! NICE! Would you like to play again? ").lower()
+def start_guessing(user_guess, rand_num):
 
-    if question in ["y","yes","yeah","yep"]:
-        guess_list.clear()
+    is_valid_input = False
 
-        new_random_number = random.randint(0, 101)
-        print(f"New random number is {new_random_number}")  # for testing
+    while not is_valid_input:
 
-        new_user_number = int(input("Okay, let's see if you can get it on your first try again!!\n"
-                                    "Enter a number here: "))
-        guess_list.append(new_user_number)
-        all_guesses.append(new_user_number)
+        try:
 
-        if new_user_number == new_random_number and len(guess_list) == 1:
+            while user_guess != rand_num:
 
-            return first_time_guess(new_user_number)
+                if user_guess < 0 or user_guess > 100:
 
-        else:
-            return wrong_number(new_user_number)
+                    user_guess = int(input(f"{user_guess} is out of specified range. Try again: "))
+                    user_guesses.append(user_guess)
+
+                elif abs(user_guess - rand_num) > 0 and abs(user_guess - rand_num) <= 5:
+
+                    user_guess = int(input(f"{user_guess} IS BURNING: "))
+                    user_guesses.append(user_guess)
+
+                elif abs(user_guess - rand_num) > 5 and abs(user_guess - rand_num) <= 10:
+
+                    user_guess = int(input(f"{user_guess} is HOT: "))
+                    user_guesses.append(user_guess)
+
+                elif abs(user_guess - rand_num) > 10 and abs(user_guess - rand_num) <= 20:
+
+                    user_guess = int(input(f"{user_guess} is REALLY WARM: "))
+                    user_guesses.append(user_guess)
+
+                elif abs(user_guess - rand_num) > 20 and abs(user_guess - rand_num) <= 30:
+
+                    user_guess = int(input(f"{user_guess} is WARMER: "))
+                    user_guesses.append(user_guess)
+
+                elif abs(user_guess - rand_num) > 30 and abs(user_guess - rand_num) <= 40:
+
+                    user_guess = int(input(f"{user_guess} is WARM: "))
+                    user_guesses.append(user_guess)
+
+                elif abs(user_guess - rand_num) > 40 and abs(user_guess - rand_num) <= 60:
+
+                    user_guess = int(input(f"{user_guess} is COLD: "))
+                    user_guesses.append(user_guess)
+
+                elif abs(user_guess - rand_num) > 60 and abs(user_guess - rand_num) <= 100:
+
+                    user_guess = int(input(f"{user_guess} is FREEZING: "))
+                    user_guesses.append(user_guess)
+
+                if user_guess == rand_num:
+
+                    response_yes = ["y", "yes", "yeah", "yep"]
+
+                    print("You guessed the right number.")
+                    print("It took you " + str(len(user_guesses)) + " tries.")
+
+                    question = input("Would you like to play again? (y, yes, yeah, yep)").lower()
+
+                    if question in response_yes:
+                        user_guesses.clear()
+                        play(random.randint(0, 101))
+                    else:
+                        print("Goodbye.")
+
+        except ValueError:
+            print("NOT VALID INPUT. Please enter another guess")
+
+
+def first_time_guess():
+
+    response_yes = ["y", "yes", "yeah", "yep"]
+
+    print("You have guessed the right number on your first try.")
+    question = input("Would you like to play again? (y, yes, yeah, yep)").lower()
+
+    if question in response_yes:
+        user_guesses.clear()
+
+        print("Okay, let's see if you can get it on your first try again.")
+        play(random.randint(0, 101))
 
     else:
-        return print("Have a wonderful day! These are your results!\n")
+        return print("Goodbye.")
 
 
-if user_number == random_number and len(guess_list) == 1:
-
-    first_time_guess(user_number)
-
-elif user_number == random_number:
-
-    right_number(user_number)
-
-else:
-
-    wrong_number(user_number)
-
-print(f"Last session guesses: {guess_list}")
-print(f"All your guesses from all your games: {all_guesses}")
+if __name__ == "__main__":
+    play(random.randint(0, 101))
